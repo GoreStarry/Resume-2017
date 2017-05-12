99380c43bd2974b4728415cb077fd659249f4345
Gore Wang <sunrise91.t3@gmail.com>
2017-05-12 22:22:36 +0800

    add contact me icon link

--- src/container/index/index.vue
+++ src/container/index/index.vue
@@ -7,6 +7,17 @@
       <div class="box__innerPlanets" :style="{initInnerHeight}">
         <planet v-for="(planet, index) in planet_datas" :planetIndex="index" :planetOpen="planet_open" :key="planet.name_en" @openPlanet="openPlanet" :name_en="planet.name_en" :name_zh="planet.name_zh" :color_planet="planet.color_planet" :color_planet_shadow="planet.color_planet_shadow" :color_name="planet.color_name"></planet>
       </div>
+      <div class="box__link">
+        <a href="mailto:sunrise91.t3@gmail.com">
+          <img src="./components/AboutMe/images/icons/envelop.svg" alt="email icon" class="icon">
+        </a>
+        <a target="_blank" href="https://github.com/GoreStarry">
+          <img src="./components/AboutMe/images/icons/github.svg" alt="github icon" class="icon">
+        </a>
+        <a href="tel:+886988104329">
+          <img src="./components/AboutMe/images/icons/phone2.svg" alt="phone icon" class="icon">
+        </a>
+      </div>
     </div>
     <div :style="{pointerEvents: planet_open ? 'initial' : 'none'}" class="box__detail">
       <img @click="closePlanet" v-show="planet_open" class="btn__closePlanet" src="./images/repeat.svg" alt="">
@@ -101,6 +112,50 @@ export default {
 
 .box__planets {
   overflow: hidden;
+  perspective-origin: center bottom;
+  perspective: 50vh;
+}
+
+$box_link_padding: 0.6rem;
+.box__link {
+  box-sizing: border-box;
+  position: absolute;
+  width: 20rem;
+  height: 3.5rem;
+  bottom: -$box_link_padding;
+  left: 0;
+  right: 0;
+  margin: auto;
+
+  padding: $box_link_padding;
+  border: solid 3px rgba(white, 0.5);
+  border-radius: 0.3rem;
+  transform: rotateX(40deg);
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  a {
+    display: inline-block;
+  }
+  img {
+    height: 100%;
+    transition: transform 0.2s linear;
+    opacity: 0.7;
+    &:hover {
+      transform: scale(1.2);
+      animation: icon_shine 0.5s infinite alternate;
+    }
+  }
+}
+
+@keyframes icon_shine {
+  from {
+    opacity: 0.5;
+  }
+  to {
+    opacity: 1;
+  }
 }
 
 .planet__active {
